42f465030ffca5838c2bd41a51180e741e08575d
xingguang2024 <xingguang@clare.ai>
2025-03-02 13:44:31 +0800

    feat: update api url

--- web/src/lib/env.ts
+++ web/src/lib/env.ts
@@ -55,7 +55,7 @@ const docs = {
     apiLicense: "https://github.com/imputnet/cobalt/blob/main/api/LICENSE",
 };
 
-const defaultApiURL = "https://api.cobalt.tools";
+const defaultApiURL = "https://api-cobalt.leeapps.dev";
 
 export { donate, defaultApiURL, contacts, partners, siriShortcuts, docs };
 export default variables;
